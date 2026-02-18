b5f28717d8adf91bfe64b90343a6b718420230e3
andrefedev <andrefedev@iMac-de-andrefedev.local>
2026-02-18 11:38:17 -0500

    w

---
+++ packages/api/src/trpc.ts
@@ -0,0 +1,10 @@
+import { initTRPC } from '@trpc/server';
+ 
+// You can use any variable name you like.
+// We use t to keep things simple.
+const t = initTRPC.create();
+
+// const t2 = initTRPC.context<{ signGuestBook: () => Promise<void> }>().create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
