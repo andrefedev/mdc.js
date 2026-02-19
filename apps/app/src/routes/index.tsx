c24aecccabd8b1020f806cfb27e13010ec217181
andrefedev <andrefedev@gmail.com>
2026-02-19 07:44:54 -0500

    w

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -1,7 +1,19 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { redirect, createFileRoute } from "@tanstack/react-router";
 
-const Index = () => {
-	return <h1>Hello World</h1>;
-};
+// const Index = () => {
+// 	return <h1>Hello World</h1>;
+// };
 
-export const Route = createFileRoute("/")({ component: Index });
+export const Route = createFileRoute("/")({
+	beforeLoad: async ({ location }) => {
+		throw redirect({
+			to: "/login",
+			search: {
+				// Use the current location to power a redirect after login
+				// (Do not use `router.state.resolvedLocation` as it can
+				// potentially lag behind the actual current location)
+				redirect: location.href,
+			},
+		});
+	},
+});
